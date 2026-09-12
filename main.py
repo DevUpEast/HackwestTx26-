7049117361a4f6981ceeaa6298d754aa14e836cf
Daniel F. <folajimifabusuyi@gmail.com>
2026-09-12 14:08:15 -0500

    Fix Flask setup and add Python dependencies

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-import Flask
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+db = SQLAlchemy(app)
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return "Welcome to HackwestTx26!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
